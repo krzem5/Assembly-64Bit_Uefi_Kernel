42e7462cdecc673041e54c86a7aa2d905ee85a58
Krzem <krzem5.dev@gmail.com>
2021-04-19 17:30:09 +0200

    Push Update 04/19/2021, 17:29:36

---
+++ build.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import os
+
+
+
+if (os.path.exists("build")):
+	dl=[]
+	for r,ndl,fl in os.walk("build"):
+		r=r.replace("\\","/").strip("/")+"/"
+		for d in ndl:
+			dl.insert(0,r+d)
+		for f in fl:
+			os.remove(r+f)
+	for k in dl:
+		os.rmdir(k)
+else:
+	os.mkdir("build")
